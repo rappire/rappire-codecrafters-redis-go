95fa37b1cf67f079834d1918b7b7b7b1547c0f39
rappire <rappire@gmail.com>
2025-08-28 23:15:40 +0900

    feat: add replconf

--- app/client/client.go
+++ app/client/client.go
@@ -131,6 +131,7 @@ func ReadRDB(reader *bufio.Reader) ([]byte, error) {
 	line = strings.TrimRight(line, "\r\n")
 
 	if len(line) == 0 || line[0] != '$' {
+
 		return nil, fmt.Errorf("invalid RDB bulk string header: %q", line)
 	}
 
